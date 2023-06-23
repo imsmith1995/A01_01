f29ba23f862f21d6635c891b489c8ec61020e7e9
Ian Smith <imsmith1995@gmail.com>
2023-06-23 13:40:25 -0400

    Add project files.

---
+++ A01_01/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A01_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a simple message for Assessment 01 of Lesson 01.");
+            MessageBox.Show("For the Assessment I added this box and added click functionality.");
+        }
+    }
+}
